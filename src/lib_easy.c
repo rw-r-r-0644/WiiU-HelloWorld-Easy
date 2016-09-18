52db1c47b02d68ffd168b8f2a15118ef3c2ac027
rw-r-r-0644 <rwrr0644@gmail.com>
2016-09-18 15:01:21 +0200

    Create lib_easy.c

---
+++ src/lib_easy.c
@@ -0,0 +1,67 @@
+void ucls() {
+  OSScreenClearBufferEx(0, 0);
+  OSScreenClearBufferEx(1, 0);
+}
+
+void ScreenInit() {
+  //Init screen and screen buffers
+  OSScreenInit();	
+  screen_buf0_size = OSScreenGetBufferSizeEx(0);
+  screen_buf1_size = OSScreenGetBufferSizeEx(1);
+  screenBuffer = MEM1_alloc(screen_buf0_size + screen_buf1_size, 0x40);
+  OSScreenSetBufferEx(0, screenBuffer);
+  OSScreenSetBufferEx(1, (screenBuffer + screen_buf0_size));
+  OSScreenEnableEx(0, 1);
+  OSScreenEnableEx(1, 1);
+  ucls(); //Clear screens
+}
+
+void flipBuffers() {
+  // Flush the cache
+  DCFlushRange(screenBuffer, screen_buf0_size);
+  DCFlushRange((screenBuffer + screen_buf0_size), screen_buf1_size);
+  // Flip buffers
+  OSScreenFlipBuffersEx(0);
+  OSScreenFlipBuffersEx(1);
+}
+
+int line;
+
+void uprintf(const char* format, ...) {
+  if(line==18) { //Out of gamepad screen
+    ucls(); //Clear Screen
+    line=0; //Reset line
+  }
+  char buff[255];
+  va_list argptr;
+	va_start(argptr, format);
+	vsnprintf(buff, 255, format, argptr);
+	va_end(argptr);
+	for(int i=0; i<2; i++) {	//Print on both Buffers
+    OSScreenPutFontEx(0, 0, line, buff);		//That is printed to TV
+    OSScreenPutFontEx(1, 0, line, buff);		//That is printed on GamePad
+    flipBuffers();
+  }
+	line++;
+}
+
+int isPressed(int button) {
+	VPADRead(0, &vpad, 1, &vpadError);
+	if(vpad.btns_h&button) return 1;
+	else return 0;
+}
+
+void uInit() {
+	//--Initialize function pointers--
+  InitOSFunctionPointers();		//Coreinit functions.
+  InitVPadFunctionPointers();		//For gamepad input
+  memoryInitialize();				//You probably shouldn't care about this for now :P
+  VPADInit();						//Init GamePad input library (needed for getting gamepad input)
+	ScreenInit();					//Init OSScreen (all the complex stuff is in easyfunctions.h :P )
+}
+
+void uDeInit() {
+	MEM1_free(screenBuffer);
+	screenBuffer = NULL;
+	memoryRelease();
+}
