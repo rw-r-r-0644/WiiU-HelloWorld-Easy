86f4c71ed4c2280afab475ae58671d54816456ad
rw-r-r-0644 <rwrr0644@gmail.com>
2016-09-18 15:10:14 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int Menu_Main(void) {
 	while(1) { 
 		//<---put stuff that should run continuously before exiting here--->
 		
-
+		updatePressedButtons(); //Update buttons state
 		if(isPressed(VPAD_BUTTON_HOME)) break; //Check if home is pressed
 	}
 	uprintf("Exiting...");
